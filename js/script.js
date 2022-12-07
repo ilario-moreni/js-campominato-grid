ffc3cab0385617fe7fa5626ff53aeddee3c6f3fd
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-07 10:00:34 +0100

    exercise done (fixed after live coding)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-alert('collegatto')
+/* 1 */
+
+const button = document.getElementById('my-button');
+
+
+
+/* 2 */
+function generateGameGrid(){
+    document.querySelector('#my-table').innerHTML = '';
+    for (let i = 0; i < 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerText = i+1;
+        cell.addEventListener('click', function(){
+            this.classList.toggle('clicked')
+            console.log(this.innerText)
+        })
+        document.querySelector('#my-table').appendChild(cell)
+    }
+}
+
+
+
+button.addEventListener('click', function(){
+    generateGameGrid()
+})
+
+/* 3 */
+
